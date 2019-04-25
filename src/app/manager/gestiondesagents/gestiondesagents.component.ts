2b8371f1bc48a1ee2bd958a1742dc1515cee3c35
jcgwebdeveloper <jc.gouleau@gmail.com>
2019-04-25 15:58:44 +0200

    mon premier essai

---
+++ src/app/manager/gestiondesagents/gestiondesagents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestiondesagents',
+  templateUrl: './gestiondesagents.component.html',
+  styleUrls: ['./gestiondesagents.component.css']
+})
+export class GestiondesagentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
