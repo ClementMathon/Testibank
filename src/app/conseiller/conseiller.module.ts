f093b03daf262e49f0ce1958d8275de05d90e86d
ClementMathon <mathon.clement.lgm@gmail.com>
2019-04-25 15:29:14 +0200

    initial

---
+++ src/app/conseiller/conseiller.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConseillerComponent } from './conseiller.component';
+
+@NgModule({
+  declarations: [ConseillerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConseillerModule { }
