1ff02f8b09835825c2f92743124deb24fd2bc06a
nieluj1981 <julien.sejourne@gmail.com>
2019-04-25 15:58:19 +0200

    julien 1

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientComponent } from './client.component';
+import { ComptesComponent } from './comptes/comptes.component';
+import { VirementComponent } from './virement/virement.component';
+import { ProfilComponent } from './profil/profil.component';
+import { HistoriqueComponent } from './historique/historique.component';
+import { CommandesComponent } from './commandes/commandes.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 @NgModule({
-  declarations: [ClientComponent],
+  declarations: [ClientComponent, ComptesComponent, VirementComponent, ProfilComponent, HistoriqueComponent, CommandesComponent, NotificationsComponent],
   imports: [
     CommonModule
   ]
