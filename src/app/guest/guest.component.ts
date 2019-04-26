62d1b93e6b3ad3abeb57f62f63976929eb66c590
ClementMathon <mathon.clement.lgm@gmail.com>
2019-04-26 12:27:59 +0200

    Unstable

--- src/app/guest/guest.component.ts
+++ src/app/guest/guest.component.ts
@@ -4,13 +4,25 @@ import { Notif } from 'src/model/notif';
 @Component({
   selector: 'app-guest',
   templateUrl: './guest.component.html',
-  styleUrls: ['./guest.component.css']
+  styleUrls: ['./guest.component.css'],
+  template:
+    '<img alt="" height="250" width="250" src={{imageSource}}>'
+  
 })
 export class GuestComponent implements OnInit {
+
+  article = new Notif();
+  imageSource: string;
+  
+  
+//image = "http://www.legorafi.fr/wp-content/uploads/2019/04/stool-217237_1280-e1556136213851.jpg";
+
 
   constructor() { }
 
   ngOnInit() {
+    //this.article.imgSrc = "http://www.legorafi.fr/wp-content/uploads/2019/04/stool-217237_1280-e1556136213851.jpg";
+    this.imageSource = this.article.imgSrc;
   }
 
 }
