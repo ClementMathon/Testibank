d386fb0266d102ac19bedc05e5f4b018156a220f
nieluj1981 <julien.sejourne@gmail.com>
2019-04-29 15:41:07 +0200

    julien maj client julio

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -9,8 +9,18 @@ import { Client } from 'src/model/client';
 })
 export class ClientComponent implements OnInit {
   client: Client = {
-    id: 1,
-    nom: 'Julio'
+  id : 1,
+	motDePasse : 'motDePasse',
+  nom: 'César',
+  prenom : 'Julio',
+	email : 'julio.caesar@rome.it',
+	telephone : '0123456789',
+	adresse : 'Capitole',
+	codePostale : 12000,
+	ville : 'Roma',
+	situation : 'empereur',
+	nombreEnfant: 2512,
+	conseiller : 1,
   };
 
   constructor() { }
