6b23b49c75e5f27f12151b3649b7ef469b46523d
Marc Ellis <mhe.mex@gmail.com>
2019-05-02 17:16:18 +0200

    modifs conseiller

--- src/app/conseiller/gestion-compte-clients/client-detail/client-detail.component.ts
+++ src/app/conseiller/gestion-compte-clients/client-detail/client-detail.component.ts
@@ -3,33 +3,25 @@ import {Client} from 'src/model/client';
 import { Compte } from 'src/model/compte';
 import {FakeServiceCompteService} from '../../../../model/fake-service-compte.service';
 
-
 @Component({
   selector: 'app-client-detail',
   templateUrl: './client-detail.component.html',
   styleUrls: ['./client-detail.component.css']
 })
 
-
 export class ClientDetailComponent implements OnInit {
   @Input() client: Client;
 
-  //compteDuClient : Client;
-
   comptesDuClient : Compte[]; 
-
+  
   constructor( private compteClientService: FakeServiceCompteService) { }
-
 
   getComptesClient(): void {
      this.compteClientService.getComptesDuClient(1).subscribe(comptesDuClient => this.comptesDuClient = comptesDuClient);
   }
 
+  // putting this.client.id instead of 1 doesn't work
   ngOnInit() {
     this.getComptesClient();
   }
-
-
-
-
 }
