9e433ba07cf0efc97216e5b08261e7501e97190a
nieluj1981 <julien.sejourne@gmail.com>
2019-05-07 21:17:42 +0200

    juju

--- src/app/client/profil/profil.component.ts
+++ src/app/client/profil/profil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Client } from 'src/model/client';
 
 @Component({
   selector: 'app-profil',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilComponent implements OnInit {
 
+  situations = ['Celibataire', 'Marie(e)',
+  'Veuf/veuve', 'Divorce', 'En cours de Brexit', 'PACse', 'Marie a un raton laveur'];
+
+  model = new Client(8, 'pwd', 'Trump', 'Donald', 'iLuvGoldenShower@potus.com', 706060656, '28 rue de GK', 69007, 'New York', this.situations[6], 0, 0 );
+
+  submitted = false;
+  
   constructor() { }
 
   ngOnInit() {
   }
+  onSubmit() { this.submitted = true; }
 
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
 }
