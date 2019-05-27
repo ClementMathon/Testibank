5c9805b175cbb5f0f44943043bb2f94aa44208b4
ClementMathon <mathon.clement.lgm@gmail.com>
2019-05-27 17:01:25 +0200

    formClient

--- src/app/guest/inscription/inscription.component.ts
+++ src/app/guest/inscription/inscription.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Client2 } from 'src/model/client2';
 import {ClientService} from "src/model/client.service"
+import { Conseiller } from 'src/model/conseiller';
 
 @Component({
   selector: 'app-inscription',
@@ -11,7 +12,19 @@ export class InscriptionComponent implements OnInit {
   newClient: Client2;
   constructor(private myClientService : ClientService) { 
     this.newClient = new Client2();
-
+    let c = new Conseiller(0,"","","","","","",0,"");
+    this.newClient.clientNom = "";
+    this.newClient.clientPrenom = "";
+    this.newClient.clientEmail = "";
+    this.newClient.clientTel = "";
+    this.newClient.clientAdresse = "";
+    this.newClient.clientCp = 0;
+    this.newClient.clientVille = "";
+    this.newClient.clientSituation = "";
+    this.newClient.client_nbEnfant = 0;
+    this.newClient.clientMdp = "";
+    this.newClient.conseiller = c;
+    
 
 
   }
