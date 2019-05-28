d9336ffad2872dc950762a98eb8767554dedf0a5
ClementMathon <mathon.clement.lgm@gmail.com>
2019-05-28 10:29:06 +0200

    ajout Inscription conseiller

--- src/app/guest/inscription/inscription.component.ts
+++ src/app/guest/inscription/inscription.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Client2 } from 'src/model/client2';
 import {ClientService} from "src/model/client.service"
 import { Conseiller } from 'src/model/conseiller';
+import { FakeServiceConseillerService } from 'src/model/fake-service-conseiller.service';
+import {Manager} from 'src/model/Manager';
 
 @Component({
   selector: 'app-inscription',
@@ -10,31 +12,37 @@ import { Conseiller } from 'src/model/conseiller';
 })
 export class InscriptionComponent implements OnInit {
   newClient: Client2;
-  constructor(private myClientService : ClientService) { 
-    this.newClient = new Client2();
-    let c = new Conseiller(0,"","","","","","",0,"");
-    this.newClient.clientNom = "";
-    this.newClient.clientPrenom = "";
-    this.newClient.clientEmail = "";
-    this.newClient.clientTel = "";
-    this.newClient.clientAdresse = "";
-    this.newClient.clientCp = 0;
-    this.newClient.clientVille = "";
-    this.newClient.clientSituation = "";
-    this.newClient.client_nbEnfant = 0;
-    this.newClient.clientMdp = "";
-    this.newClient.conseiller = c;
-    
+  myNewAdvisor: Conseiller;
+  newAdmin: Manager;
+  constructor(private myClientService : ClientService, private myConseillerService : FakeServiceConseillerService) { 
+  this.newClient = new Client2();
 
-
+  this.newClient.clientAdresse = "";
+  this.newClient.clientCp = 12345;
+  this.newClient.clientEmail = "mail";
+  this.newClient.clientId = 0;
+  this.newClient.clientMdp = "pwd";
+  this.newClient.clientNom = "Lovelace";
+  this.newClient.clientPrenom = "Ada";
+  this.newClient.clientSituation = "e";
+  this.newClient.clientTel = "0123456789";
+  this.newClient.clientVille = "oui";
+  this.newClient.client_nbEnfant = 0;
+  this.newClient.conseiller = this.myNewAdvisor;
   }
+
+   
 
   ngOnInit() {
-   
+    this.myClientService.addClient(this.newClient);
   }
 
-  AddAClient(){
+  AddClient(){
     this.myClientService.addClient(this.newClient);
+  }
+
+  addConseiller(){
+    this.myConseillerService.addCounselor(this.myNewAdvisor);
   }
   
 
