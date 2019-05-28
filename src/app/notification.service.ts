dc98fe1b07aaf7b7b7243c2be967ab0d2d1029c0
nieluj1981 <julien.sejourne@gmail.com>
2019-05-28 14:39:23 +0200

    test

---
+++ src/app/notification.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Client2 } from 'src/model/client2';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  Client: Client2;
+  uri = 'http://localhost:8080/Testibank/compte';
+
+  constructor(private http: HttpClient) { }
+
+  notifCommandeChequier(targetClientID) {
+    const uri2 = this.uri+ '/findAllComptesByClientId/' + targetClientID;
+    return this.http.get<Compte[]>(uri2).pipe(retry(1), catchError(this.handleError));
+  }
+
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+
+    }
+}
