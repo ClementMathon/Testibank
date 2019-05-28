044c57d3be414aecdf01156e7c601a8382187f64
ClementMathon <mathon.clement.lgm@gmail.com>
2019-05-28 14:19:07 +0200

    auth

---
+++ src/model/auth.ts
@@ -0,0 +1,10 @@
+import { Client2 } from './client2';
+import { Conseiller } from './conseiller';
+import { Manager } from './manager';
+
+export class Auth{
+    client : Client2;
+    conseiller : Conseiller;
+    admin : Manager;
+    constructor(){};
+}
